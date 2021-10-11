c72b766ce471de090cf9caf88dd45e9959870001
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-10-11 16:49:41 +0530

    Migrate to @Resource annotation for injecting the pool

--- src/main/java/lk/ijse/dep7/fileuploadingbackend/StudentServlet.java
+++ src/main/java/lk/ijse/dep7/fileuploadingbackend/StudentServlet.java
@@ -1,14 +1,41 @@
 package lk.ijse.dep7.fileuploadingbackend;
 
+import jakarta.annotation.Resource;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
 
+import javax.naming.InitialContext;
+import javax.naming.NameClassPair;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.sql.SQLException;
 
 @MultipartConfig
-@WebServlet(name = "StudentServlet", value = "/students")
+@WebServlet(name = "StudentServlet", value = "/students", loadOnStartup = 0)
 public class StudentServlet extends HttpServlet {
+
+    @Resource(name = "java:comp/env/jdbc/cp")
+    private DataSource dataSource;
+
+    @Override
+    public void init() throws ServletException {
+//        try {
+//            InitialContext ctx = new InitialContext();
+//            DataSource ds = (DataSource) ctx.lookup("java:comp/env/jdbc/cp");
+//            System.out.println(ds.getConnection());
+//        } catch (NamingException | SQLException e) {
+//            e.printStackTrace();
+//        }
+        try {
+            System.out.println(dataSource.getConnection());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("Hello Servlet");
