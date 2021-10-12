af4ceaa3392aabcffdbe962bf4f3f6d349abea4c
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2021-10-12 18:58:01 +0530

    Revamp with SQL Boolean Mode

--- src/main/java/lk/ijse/dep7/fileuploadingbackend/service/StudentService.java
+++ src/main/java/lk/ijse/dep7/fileuploadingbackend/service/StudentService.java
@@ -5,6 +5,7 @@ import lk.ijse.dep7.fileuploadingbackend.dto.StudentDTO;
 import javax.sql.rowset.serial.SerialBlob;
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class StudentService {
@@ -43,14 +44,33 @@ public class StudentService {
 
         List<StudentDTO> students = new ArrayList<>();
 
-        query = '%' + query + '%';
+//        query = '%' + query + '%';
+
+        /* Gall Di 078 */
+        String[] split = query.split("\\s");
+
+        System.out.println(Arrays.toString(split));
+        query = "";
+
+        for (String s : split) {
+            query += "+" + s + "* ";
+        }
 
         try {
-            PreparedStatement stm = connection.prepareStatement("SELECT * FROM student WHERE id LIKE ? OR name LIKE ? or address LIKE ? or contact LIKE ?");
-            stm.setString(1, query);
-            stm.setString(2, query);
-            stm.setString(3, query);
-            stm.setString(4, query);
+
+            String sql = "SELECT * FROM student WHERE MATCH (name, address, contact) AGAINST (? IN BOOLEAN MODE);";
+            if (query.trim().equals("+*")){
+                sql = "SELECT * FROM student";
+                query = null;
+            }
+
+            PreparedStatement stm = connection.prepareStatement(sql);
+//            PreparedStatement stm = connection.prepareStatement("SELECT * FROM student WHERE id LIKE ? OR name LIKE ? or address LIKE ? or contact LIKE ?");
+            if (query != null)
+            stm.setString(1, query.trim());
+//            stm.setString(2, query);
+//            stm.setString(3, query);
+//            stm.setString(4, query);
             ResultSet rst = stm.executeQuery();
 
             while (rst.next()) {
